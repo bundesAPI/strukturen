ddf3814f3383aaa551949c384834341d4611aa65
Lilith Wittmann <mail@lilithwittmann.de>
2021-12-20 02:24:34 +0100

    sentry

--- settings/configs/production.py
+++ settings/configs/production.py
@@ -12,6 +12,21 @@ DATABASES = {
     },
 }
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
