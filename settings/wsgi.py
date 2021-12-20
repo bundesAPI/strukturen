4a3d27bb52e26b0d769af4374a5f499c5697b0e4
Lilith Wittmann <mail@lilithwittmann.de>
2021-12-20 01:29:22 +0100

    add credential config

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.configs.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.configs.production")
 
 application = get_wsgi_application()
