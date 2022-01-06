824e7a6cea87d5ebb0d1fe19aeb97fcde55fb7b0
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-06 22:48:21 +0100

    actually register signals

--- orgcharts/apps.py
+++ orgcharts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OrgchartsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orgcharts"
+
+    def ready(self):
+        from . import signals
